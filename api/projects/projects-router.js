612fa3f9437564831240b61ab808d6a709273573
Calvin K <calvinknaub@gmail.com>
2023-12-11 17:02:46 -0800

    passing tests /api/projects

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,19 @@
 // Write your "projects" router here!
+const express = require("express");
+const Project = require("./projects-model");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Project.get()
+    .then((stuff) => {
+      res.json(stuff)
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "You have hit the sad path",
+      });
+    });
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -3,12 +3,13 @@
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 const express = require('express');
-
+const projectRouter = require('./projects/projects-router')
 const server = express();
 
 server.use(express.json())
+server.use('/api/projects', projectRouter)
 
-server.get('/', (req, res) => {
+server.use('*', (req, res) => {
     res.send(`Hello World`)
 })
 module.exports = server;
