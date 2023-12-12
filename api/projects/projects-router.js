ea314f410c844754d958cd65150b0ff17d93a66a
Calvin K <calvinknaub@gmail.com>
2023-12-11 18:10:33 -0800

    wip post

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get("/", (req, res) => {
   Project.get()
     .then((stuff) => {
-      res.json(stuff)
+      res.json(stuff);
     })
     .catch((err) => {
       res.status(500).json({
@@ -16,21 +16,44 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get('/:id', async (req, res) => {
-    try {
-        const task = await Project.get(req.params.id)
-        if (!task) {
-            res.status(404).json({
-                message: "Id not found"
-            })
-        } else {
-            res.json(task)
-        }
-    } catch (err) {
-        res.status(500).json({
-            message: "whoops sad path"
-        })
+router.get("/:id", async (req, res) => {
+  try {
+    const task = await Project.get(req.params.id);
+    if (!task) {
+      res.status(404).json({
+        message: "Id not found",
+      });
+    } else {
+      res.json(task);
     }
-})
+  } catch (err) {
+    res.status(500).json({
+      message: "whoops sad path",
+    });
+  }
+});
+
+router.post("/", (req, res) => {
+  const { name, description } = req.body;
+  if (!name || !description) {
+    return res.status(400).json({
+      message: "Please provide a name and/or description",
+    });
+  }
+
+  Project.insert({ name, description })
+    .then(({ id }) => {
+      return Project.get(id);
+    })
+    .then((project) => {
+      res.status(201).json(project);
+      console.log(project);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "whoops a doosie",
+      });
+    });
+});
 
 module.exports = router;
