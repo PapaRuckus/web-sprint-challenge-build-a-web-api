0a665ba583b19b077b3af6ed9e35a1025520378d
Calvin K <calvinknaub@gmail.com>
2023-12-13 11:55:33 -0800

    tests pasts /api/projects/:id put

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -42,14 +42,50 @@ router.post("/", (req, res) => {
   } else {
     Project.insert({ name, description, completed })
       .then((project) => {
-          res.status(201).json(project);
+        res.status(201).json(project);
         console.log(project);
       })
-      .catch((err) => { //eslint-disable-line
+      .catch((err) => {
         res.status(500).json({
           message: "whoops a doosie",
         });
       });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { name, description, completed } = req.body;
+    const updatedProject = await Project.get(req.params.id);
+
+    if (
+      name === undefined ||
+      description === undefined ||
+      completed === undefined
+    ) {
+      res.status(400).json({
+        message: "Missing name, description, and/or completed",
+      });
+    } else if (!updatedProject) {
+      res.status(404).json({
+        message: "Project with this id doesn't exist",
+      });
+    } else {
+      const updated = await Project.update(req.params.id, req.body);
+
+      if (!updated) {
+        res.status(500).json({
+          message: "Failed to update the project",
+        });
+      } else {
+        const updatedProject = await Project.get(req.params.id);
+        res.status(200).json(updatedProject);
+      }
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Error in the update operation",
+    });
   }
 });
 
