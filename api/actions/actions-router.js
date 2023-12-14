8d5907146bc3b22347984cb7418fb807363ba5d3
Calvin K <calvinknaub@gmail.com>
2023-12-14 13:04:54 -0800

    test passing for put actions

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -52,4 +52,35 @@ router.post("/", (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  try {
+    const { notes, description, project_id } = req.body;
+    const updatedAction = await Actions.get(req.params.id);
+
+    if (!notes || !description || !project_id) {
+      res.status(400).json({
+        message: "Please provide notes and description",
+      });
+    } else if (!updatedAction) {
+      res.status(404).json({
+        message: "Action with Id doesnt exist",
+      });
+    } else {
+      const updated = await Actions.update(req.params.id, req.body);
+      if (!updated) {
+        res.status(500).json({
+          message: "Failed yo update",
+        });
+      } else {
+        const updatedAction = await Actions.get(req.params.id);
+        res.status(200).json(updatedAction);
+      }
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Error in the update operation",
+    });
+  }
+});
+
 module.exports = router;
